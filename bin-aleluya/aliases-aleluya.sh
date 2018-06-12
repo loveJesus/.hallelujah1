4feec3387eb3bd8aa2a6575d349484dc90f6cc4f
loveJesus <loveJesus@loveJesus.xyz>
2018-06-11 22:05:32 -0400

    Hallelujah - server things /pf/nc

--- bin-aleluya/aliases-aleluya.sh
+++ bin-aleluya/aliases-aleluya.sh
@@ -1,12 +1,16 @@
+#!/bin/bash
 #hallelujah
 #convert -size 2400x1200 xc:White -gravity Center -weight 700 -pointsize 200  -annotate 0 "$2" $3
+
+alias ddp_aleluya='sed -r "/^:/, \$d" | sed "/^\s*$/d" | sed "/^(ESV2011)$/d" | awk "/^[a-zA-Z0-9 ]+:[0-9]+:/ {if (NR!=1){printf \"\n%s\",\$0;}else printf \"%s\",\$0;next; }{printf \" %s\",\$0;}END{print \"\";}" | sed -r "s/\s+/ /g"'
+
 function titleImgAleluya {
   convert "$1" -font /usr/share/fonts/TTF/DejaVuSans.ttf -stroke black -fill white -gravity Center -weight 700 -pointsize 30  -annotate 0 "$2" "$3"
 }
 
 alias bblAleluya='diatheke -f plain -b ESV2011 -k '
 alias xclip='xclip -selection clipboard '
-alias ddp_aleluya='sed "/^\s*$/d" | sed "/^(ESV2011)$/d" | awk "/^[a-zA-Z0-9 ]+:[0-9]+:/ {if (NR!=1){printf \"\n%s\",\$0;}else printf \"%s\",\$0;next; }{printf \" %s\",\$0;}END{print \"\";}" | sed -r "s/\s+/ /g"'
+
 function bbldAleluya {
   bblAleluya $@ | ddp_aleluya
 }
@@ -23,12 +27,16 @@ function postit_aleluya {
   nxxd_aleluya=`curl -X POST -H "Content-Type: $2" --data-binary "@$1"  https://loveJesus.xyz/sha-aleluya/input-aleluya.php\?lnk-aleluya\=$3 | xxd -ps | paste -sd ''`
   echo $nxxd_aleluya | tee -a ~/lposts_aleluya
 }
-
+function dreamhost_api_aleluya {
+  cmd_aleluya="${1:-api-list_accessible_cmds}"  
+  curl "https://api.dreamhost.com/?key=$DREAMHOST_API_KEY_aleluya&unique_id=`date | md5sum | cut -d' ' -f1`&format=tab&cmd=$cmd_aleluya"
+}
 function readit_aleluya {
   curl https://loveJesus.xyz/sha-aleluya/$1
 }
 
 function refreshNameCheapDomains_aleluya {
+  #Hallelujah for dananos @  https://blog.dotnetframework.org/2016/04/15/how-to-export-a-list-of-domains-from-namecheap/ in Jesus name
 curl "https://api.namecheap.com/xml.response?ApiUser=brianloveJesus&ApiKey=$NAMECHEAP_API_KEY_aleluya&UserName=brianloveJesus&Command=namecheap.domains.getList&ClientIp=$NAMECHEAP_CLIENT_IP_aleluya&PageSize=100" | ~/bin-aleluya/xml2json-aleluya.py | python -mjson.tool >| domains-nc-aleluya.json
 #cat domains-nc-aleluya.json| grep @Name | cut -d':' -f 2 | tr -d , | tr -d \" | while read domain_aleluya; do sld_aleluya=`echo $domain_aleluya | cut -d'.' -f 1`; tld_aleluya=`echo $domain_aleluya | cut -d'.' -f 2`; curl "https://api.namecheap.com/xml.response?ApiUser=brianloveJesus&ApiKey=$NAMECHEAP_API_KEY_aleluya&UserName=brianloveJesus&ClientIp=$NAMECHEAP_CLIENT_IP_aleluya&PageSize=100&Command=namecheap.domains.dns.getList&SLD=sld_aleluya&TLD=$tld_aleluya" > domains-aleluya/$domain_aleluya.aleluya; echo ALELUYA $domain_aleluya; done
 
@@ -37,6 +45,19 @@ curl "https://api.namecheap.com/xml.response?ApiUser=brianloveJesus&ApiKey=$NAME
 function jsonPP_aleluya {
 	cat $1 | python -m json.tool | pygmentize
 }
+function nameCheapSetCustom_aleluya {
+  sld_aleluya=$1
+  tld_aleluya=$2
+  ns_aleluya=$3
+  curl "https://api.namecheap.com/xml.response?ApiUser=brianloveJesus&ApiKey=$NAMECHEAP_API_KEY_aleluya&UserName=brianloveJesus&ClientIp=$NAMECHEAP_CLIENT_IP_aleluya&PageSize=100&Command=namecheap.domains.dns.setCustom&SLD=$sld_aleluya&TLD=$tld_aleluya&NameServers=$ns_aleluya"  
+
+}
+
+function nameCheapFilesDisplay_aleluya {
+  for aleluya in *.aleluya; do echo '~' ; echo ALELUYA $aleluya ---- ; cat $aleluya | grep Nameserver | sed -r 's/.*>(.*)<.*/\1/g'; done | paste -sd' ' | tr '~' '\n' 
+}
+
+
 function noSshKeyCheckAleluya {
     # Add the servers to the sshloginfile
   (echo servera; echo serverb) > .parallel/my_cluster
--- bin-aleluya/startup-aleluya.sh
+++ bin-aleluya/startup-aleluya.sh
@@ -1,7 +1,8 @@
-#!Hallelujah
+#!/bin/bash
+#Hallelujah
 export PATH=$PATH:~/apps-aleluya/kotlinc-aleluya/bin:~/apps-aleluya/kotlin-native-aleluya/bin
 export LESS='-R'
 export LESSOPEN='|~/.lessfilter %s'
-export PATH="$HOME/.cargo/bin:$PATH"
-source ~/.envhere-aleluya
-source ~/bin-aleluya/aliases-aleluya.sh
+export PATH="$HOME/bin-aleluya:$HOME/.cargo/bin:$PATH"
+.  ~/.envhere-aleluya
+. ~/bin-aleluya/aliases-aleluya.sh
