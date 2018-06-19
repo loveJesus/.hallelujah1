865a0b9527389d384cd960adb3fd68eaecbb51df
loveJesus <loveJesus@loveJesus.xyz>
2018-06-18 18:56:03 -0600

    Hallelujah - more NC things

--- bin-aleluya/aliases-aleluya.sh
+++ bin-aleluya/aliases-aleluya.sh
@@ -39,13 +39,14 @@ function nameCheapRefreshDomains_aleluya {
   #Hallelujah for dananos @  https://blog.dotnetframework.org/2016/04/15/how-to-export-a-list-of-domains-from-namecheap/ in Jesus name
 curl "https://api.namecheap.com/xml.response?ApiUser=brianloveJesus&ApiKey=$NAMECHEAP_API_KEY_aleluya&UserName=brianloveJesus&Command=namecheap.domains.getList&ClientIp=$NAMECHEAP_CLIENT_IP_aleluya&PageSize=100" | ~/bin-aleluya/xml2json-aleluya.py | python -mjson.tool >| domains-nc-aleluya.json
 if [ "$1" = aleluya ]; then
- cat domains-nc-aleluya.json| grep @Name | cut -d':' -f 2 | tr -d , | tr -d \" | while read domain_aleluya; do 
+  mkdir -p domains-aleluya
+  nohup cat domains-nc-aleluya.json| grep @Name | cut -d':' -f 2 | tr -d , | tr -d \" | while read domain_aleluya; do 
    sld_aleluya=`echo $domain_aleluya | cut -d'.' -f 1`; 
    tld_aleluya=`echo $domain_aleluya | cut -d'.' -f 2`; 
-   curl "https://api.namecheap.com/xml.response?ApiUser=brianloveJesus&ApiKey=$NAMECHEAP_API_KEY_aleluya&UserName=brianloveJesus&ClientIp=$NAMECHEAP_CLIENT_IP_aleluya&PageSize=100&Command=namecheap.domains.dns.getList&SLD=sld_aleluya&TLD=$tld_aleluya" > domains-aleluya/$domain_aleluya.aleluya &; 
+   curl "https://api.namecheap.com/xml.response?ApiUser=brianloveJesus&ApiKey=$NAMECHEAP_API_KEY_aleluya&UserName=brianloveJesus&ClientIp=$NAMECHEAP_CLIENT_IP_aleluya&PageSize=100&Command=namecheap.domains.dns.getList&SLD=$sld_aleluya&TLD=$tld_aleluya" >| domains-aleluya/$domain_aleluya.aleluya &; 
    echo ALELUYA $domain_aleluya; 
    sleep 0.1
- done
+ done 
 fi
 }
 
