742f2a292b435a4744365a2d048630d45495f4e0
loveJesus <loveJesus@loveJesus.xyz>
2018-06-21 18:04:31 -0600

    Hallelujah - bootscript 1

---
+++ bin-aleluya/onboot-aleluya.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -l
+# For God so loved the world, that He gave His only begotten Son
+# That all who believe in Him should not perish, but have everlasting life
+
+# add boot scripts in background
+
+while true; do
+  echo God is good
+  sleep 0.5
+  echo Praise Jesus
+  sleep 0.4
+done
